1c2f0c1edc526385f49195d06e3f43f3eb236f39
Léo <ldos_rj2008@hotmail.com>
2021-10-04 14:49:01 -0300

    Aula 13.

--- src/services/AspnetMicroservices.Catalog.API/Startup.cs
+++ src/services/AspnetMicroservices.Catalog.API/Startup.cs
@@ -1,15 +1,11 @@
+using AspnetMicroservices.Catalog.API.Data;
+using AspnetMicroservices.Catalog.API.Repositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace AspnetMicroservices.Catalog.API
 {
@@ -21,8 +17,7 @@ namespace AspnetMicroservices.Catalog.API
         }
 
         public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
+        
         public void ConfigureServices(IServiceCollection services)
         {
 
@@ -31,9 +26,11 @@ namespace AspnetMicroservices.Catalog.API
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "AspnetMicroservices.Catalog.API", Version = "v1" });
             });
+
+            services.AddScoped<ICatalogContext, CatalogContext>();
+            services.AddScoped<IProductRepository, ProductRepository>();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
