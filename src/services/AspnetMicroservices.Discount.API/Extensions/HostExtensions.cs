aa741020d42d6b240f4e5b1b3481fe0777622d1a
Léo <ldos_rj2008@hotmail.com>
2021-10-06 09:12:46 -0300

    Aula 15.

--- src/services/AspnetMicroservices.Discount.API/Extensions/HostExtensions.cs
+++ src/services/AspnetMicroservices.Discount.API/Extensions/HostExtensions.cs
@@ -37,7 +37,7 @@ namespace AspnetMicroservices.Discount.API.Extensions
                     command.CommandText = @"CREATE TABLE Coupon(Id SERIAL PRIMARY KEY,
                                                                 ProductName VARCHAR(24) NOT NULL,
                                                                 Description TEXT,
-                                                                Amount INT";
+                                                                Amount INT)";
 
                     command.ExecuteNonQuery();
 
