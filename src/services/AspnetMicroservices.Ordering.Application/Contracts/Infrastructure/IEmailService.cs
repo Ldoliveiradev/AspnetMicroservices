9cc53c7d967ae84426f599014c66024e21b0220e
Léo <ldos_rj2008@hotmail.com>
2021-10-06 18:19:47 -0300

    Aula 16.

---
+++ src/services/AspnetMicroservices.Ordering.Application/Contracts/Infrastructure/IEmailService.cs
@@ -0,0 +1,10 @@
+﻿using AspnetMicroservices.Ordering.Application.Models;
+using System.Threading.Tasks;
+
+namespace AspnetMicroservices.Ordering.Application.Contracts.Infrastructure
+{
+    public interface IEmailService
+    {
+        Task<bool> SendEmail(Email email);
+    }
+}
