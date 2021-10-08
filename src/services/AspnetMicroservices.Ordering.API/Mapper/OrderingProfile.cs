5b0ec4f8bf48193a81d2be59fc84106f5d5f1266
Léo <ldos_rj2008@hotmail.com>
2021-10-08 09:16:35 -0300

    Aula 12.

---
+++ src/services/AspnetMicroservices.Ordering.API/Mapper/OrderingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AspnetMicroservices.EventBus.Messages.Events;
+using AspnetMicroservices.Ordering.Application.Features.Orders.Commands.CheckoutOrder;
+using AutoMapper;
+
+namespace AspnetMicroservices.Ordering.API.Mapper
+{
+    public class OrderingProfile : Profile
+    {
+        public OrderingProfile()
+        {
+            CreateMap<CheckoutOrderCommand, BasketCheckoutEvent>().ReverseMap();
+        }
+    }
+}
