03ecfacebf5bc8856fd6e38c10ade7863efae5f6
Léo <ldos_rj2008@hotmail.com>
2021-10-08 07:53:12 -0300

    Aula 08.

--- src/services/AspnetMicroservices.Basket.API/Startup.cs
+++ src/services/AspnetMicroservices.Basket.API/Startup.cs
@@ -1,6 +1,7 @@
 using AspnetMicroservices.Basket.API.GrpcServices;
 using AspnetMicroservices.Basket.API.Repositories;
 using AspnetMicroservices.Discount.Grpc.Protos;
+using MassTransit;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -32,6 +33,16 @@ namespace AspnetMicroservices.Basket.API
             services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>(o => o.Address = new Uri(Configuration["GrpcSettings:DiscountUrl"]));
 
             services.AddScoped<DiscountGrpcService>();
+
+            services.AddMassTransit(config =>
+            {
+                config.UsingRabbitMq((ctx, cfg) =>
+                {
+                    cfg.Host(Configuration["EventBusSettings:HostAddress"]);
+                });
+            });
+
+            services.AddMassTransitHostedService();
 
             services.AddControllers();
 
